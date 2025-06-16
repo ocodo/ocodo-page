0610c261fff712705810354237e1bab430e0f93f
jasonm23 <jasonm23@gmail.com>
2025-06-16 14:51:05 +0700

    Theme-switch dark neutral 800 background in dark mode.

--- src/components/theme-switch.tsx
+++ src/components/theme-switch.tsx
@@ -26,10 +26,10 @@ function ThemeSwitch({
     <SwitchPrimitive.Root
       data-slot="switch"
       className={cn(
-        "peer data-[state=checked]:bg-primary data-[state=unchecked]:bg-input",
+        "peer data-[state=checked]:bg-neutral-800 data-[state=unchecked]:bg-input",
         "focus-visible:border-ring focus-visible:ring-ring/50 dark:data-[state=unchecked]:bg-input/80",
         "inline-flex h-[1.15rem] w-8 shrink-0 cursor-pointer items-center rounded-full",
-        "relative border border-transparent shadow-xs transition-colors outline-none",
+        "relative border border-transparent transition-colors outline-none",
         "focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50",
         className
       )}
